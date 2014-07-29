5a605184aeceb0fcb21dad6a5e33d7aceb0a2a1b
franpena <fnapster@gmail.com>
2014-07-29 11:12:19 +0100

    Modified the DummyPredictor class so it extends the BaseRecommender class

--- source/python/tripadvisor/fourcity/dummy_predictor.py
+++ source/python/tripadvisor/fourcity/dummy_predictor.py
@@ -1,10 +1,17 @@
+from tripadvisor.fourcity.base_recommender import BaseRecommender
+
 __author__ = 'fpena'
 
-class DummyPredictor:
 
-    def __init__(self, reviews):
-        pass
+class DummyPredictor(BaseRecommender):
+
+    def __init__(self, rating):
+        super(DummyPredictor, self).__init__('DummyRecommender', None)
+        self._rating = rating
 
     def predict_rating(self, user_id, hotel_id):
 
-        return 4.0
+        return self._rating
+
+    def load(self, reviews):
+        pass
