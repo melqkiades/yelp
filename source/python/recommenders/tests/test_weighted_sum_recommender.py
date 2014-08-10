e02c3131c229219001edbfd3dd1c0b7a49d8ba5d
franpena <fnapster@gmail.com>
2014-08-10 17:03:32 +0100

    Removed the test against dummy unit tests

--- source/python/recommenders/tests/test_weighted_sum_recommender.py
+++ source/python/recommenders/tests/test_weighted_sum_recommender.py
@@ -87,21 +87,3 @@ class TestWeightedSumRecommender(TestCase):
         self.assertEqual(actual_rating_1, recommender.predict_rating('A1', 1))
         actual_rating_2 = 2.0
         self.assertEqual(actual_rating_2, recommender.predict_rating('A1', 3))
-
-    def test_compare_against_dummy_recommender(self):
-        recommender = WeightedSumRecommender(SingleSimilarityMatrixBuilder('cosine'))
-        recommender.load(reviews_matrix_5)
-        _, errors = four_city_evaluator.predict_rating_list(recommender, reviews_matrix_5)
-        wsr_mean_absolute_error = MeanAbsoluteError.compute_list(errors)
-        wsr_root_mean_square_error = RootMeanSquareError.compute_list(errors)
-        print('Mean Absolute error:', wsr_mean_absolute_error)
-        print('Root mean square error:',  wsr_root_mean_square_error)
-
-        recommender = DummyRecommender(6.0)
-        _, errors = four_city_evaluator.predict_rating_list(recommender, reviews_matrix_5)
-        dummy_mean_absolute_error = MeanAbsoluteError.compute_list(errors)
-        dummy_root_mean_square_error = RootMeanSquareError.compute_list(errors)
-        print('Mean Absolute error:', dummy_mean_absolute_error)
-        print('Root mean square error:',  dummy_root_mean_square_error)
-
-        self.assertLess(wsr_mean_absolute_error, dummy_mean_absolute_error)
