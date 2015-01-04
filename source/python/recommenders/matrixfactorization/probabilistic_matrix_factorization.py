60e9cefc06dfb38c2391ae1e5587d41461560afe
franpena <fnapster@gmail.com>
2015-01-04 19:20:08 +0000

    Updated the Probabilistic Matrix Factorization class, now is working and it's showing good results

--- source/python/recommenders/matrixfactorization/probabilistic_matrix_factorization.py
+++ source/python/recommenders/matrixfactorization/probabilistic_matrix_factorization.py
@@ -1,4 +1,3 @@
-import random
 import time
 import logging
 from tripadvisor.fourcity import movielens_extractor
@@ -18,8 +17,8 @@ class ProbabilisticMatrixFactorization():
 
         # We define the variables necessary to build the matrix
         self.epsilon = 50.0
-        self.var_lambda = 0.001
-        self.momemtum = 0.8
+        self.var_lambda = 0.01
+        self.momentum = 0.8
         self.alpha = 0.0001
 
         self.max_rating = 5
@@ -38,6 +37,7 @@ class ProbabilisticMatrixFactorization():
         self.train = None
         self.validation = None
         self.num_batches = 9
+        self.batch_size = None
         self.num_items = 3952  # Can be calculated
         self.num_users = 6040  # Can be calculated
         self.num_features = 10
@@ -56,8 +56,8 @@ class ProbabilisticMatrixFactorization():
         self.num_users = movielens_extractor.get_num_users(self.train)
         self.num_items = movielens_extractor.get_num_items(self.train)
         self.mean_rating = movielens_extractor.get_mean_rating(self.train)
-        batch_size = int(np.ceil(float(len(self.train) / self.num_batches)))
-        self.alpha = self.epsilon / batch_size
+        self.batch_size = int(np.ceil(float(len(self.train) / self.num_batches)))
+        self.alpha = self.epsilon / self.batch_size
 
         self.logger.debug('epsilon: %f', self.epsilon)
         self.logger.debug('alpha: %f', self.alpha)
@@ -65,66 +65,63 @@ class ProbabilisticMatrixFactorization():
         # latent variables
         self.user_features = 0.1 * np.random.rand(self.num_users, self.num_features)
         self.item_features = 0.1 * np.random.rand(self.num_items, self.num_features)
+        user_features_inc = np.zeros((self.num_users, self.num_features))
+        item_features_inc = np.zeros((self.num_items, self.num_features))
 
         converge = 1e-4
         last_rmse = None
 
         for epoch in xrange(self.num_epochs):
 
+            # In each cycle the training vector is shuffled
+            np.random.shuffle(self.train)
+
             for batch in xrange(self.num_batches):
-                data = self.train[batch * batch_size: (batch + 1) * batch_size]
+                data = self.train[batch * self.batch_size: (batch + 1) * self.batch_size]
+                users = data[:, 0]
+                items = data[:, 1]
+                ratings = data[:, 2]
 
-                # print "batch_size", batch_size
-                # print "train", train.shape
-                # print "data", data.shape
+                 # Default prediction is the mean rating
+                ratings = ratings - self.mean_rating
 
-                # compute gradient
-                user_features = self.user_features[data[:, 0], :]
-                item_features = self.item_features[data[:, 1], :]
-                # print "u_feature", user_features.shape
-                # print "i_feature", item_features.shape
+                # compute predictions
+                predicted_ratings = np.sum(self.user_features[users, :] * self.item_features[items, :], 1)
 
-                ratings = data[:, 2] - self.mean_rating
-                predictions = np.sum(user_features * item_features, 1)
-                error_list = predictions - ratings
-
-                # print "predictions", predictions.shape
-                # print "errs", error_list.shape
-
+                # compute gradients
+                error_list = predicted_ratings - ratings
                 error_matrix = np.tile(error_list, (self.num_features, 1)).T
-                # print "err_matrix", error_matrix.shape
 
-                user_gradients =\
-                    user_features * error_matrix + self.var_lambda * item_features
                 item_gradients =\
-                    item_features * error_matrix + self.var_lambda * user_features
+                    error_matrix * self.user_features[users, :] + self.var_lambda * self.item_features[items, :]
+                user_gradients =\
+                    error_matrix * self.item_features[items, :] + self.var_lambda * self.user_features[users, :]
 
                 user_feature_gradients =\
                     np.zeros((self.num_users, self.num_features))
                 item_feature_gradients =\
                     np.zeros((self.num_items, self.num_features))
+                # In the above line the gradient is calculated for every rating,
+                #  but it has to be grouped (by summing it) for each user and
+                # item features
 
-                for i in xrange(data.shape[0]):
-                    user = data[i, 0]
-                    item = data[i, 1]
-                    user_feature_gradients[user, :] += user_gradients[i, :]
-                    item_feature_gradients[item, :] += item_gradients[i, :]
+                for i in xrange(self.batch_size):
+                    user_feature_gradients[users[i], :] += user_gradients[i, :]
+                    item_feature_gradients[items[i], :] += item_gradients[i, :]
 
-                # Update movie and user features
-
-                self.user_features = self.user_features - \
+                # Update item and user features
+                # The update is done using the momentum technique of gradient descent
+                user_features_inc = self.momentum * user_features_inc +\
                     self.alpha * user_feature_gradients
-                self.item_features = self.item_features - \
-                    self.alpha * item_feature_gradients
+                # self.user_features = self.user_features - \
+                #     self.alpha * user_feature_gradients
+                self.user_features = self.user_features - user_features_inc
 
-                # print('epsilon', self.epsilon)
-                # print('batch size', batch_size)
-                # print('user_feature_gradients', user_feature_gradients)
-                # print('gradient', ((self.epsilon / batch_size) * user_feature_gradients))
-                # print('user features')
-                # print(self.user_features[0])
-                # print(self.user_features[10])
-                # print(self.user_features[100])
+                item_features_inc = self.momentum * item_features_inc +\
+                    self.alpha * item_feature_gradients
+                # self.item_features = self.item_features - \
+                #     self.alpha * item_feature_gradients
+                self.item_features = self.item_features - item_features_inc
 
             # compute RMSE
             # train errors
@@ -160,10 +157,6 @@ class ProbabilisticMatrixFactorization():
         if self.min_rating:
             predictions[predictions < self.min_rating] = self.min_rating
 
-        # print(predictions[0])
-        # print(predictions[10])
-        # print(predictions[100])
-
         return predictions
 
 
@@ -183,6 +176,7 @@ def example():
     reviews = movielens_extractor.get_ml_1m_dataset()
     ratings = movielens_extractor.reviews_to_numpy_matrix(reviews)
     # suffle_data
+    np.random.seed(0)
     np.random.shuffle(ratings)
 
     # split data to training & validation
@@ -192,7 +186,6 @@ def example():
     validation = ratings[train_size:]
 
     # params
-    num_feature = 10
     bmf_model = ProbabilisticMatrixFactorization()
 
     start_time = time.clock()
@@ -205,16 +198,3 @@ def example():
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s %(asctime)s %(message)s')
 example()
 
-
-# random.seed(0)
-#
-# a = [0, 1, 2, 3, 4, 5]
-# random.shuffle(a)
-# print(a)
-# np.random.seed(0)
-# print (np.random.permutation(range(6)))
-#
-# print(np.random.random())
-# print(np.random.random())
-# print(np.random.random())
-# print(np.random.random())
