35a35666492bca5be43aa1250c640ee81b74914f
franpena <fnapster@gmail.com>
2015-02-02 19:46:43 +0000

    Added the initial version of the makematrix.m code.

--- source/python/recommenders/matrixfactorization/makematrix.m
+++ source/python/recommenders/matrixfactorization/makematrix.m
@@ -22,20 +22,6 @@ num_m = 3952;
 num_p = 6040;
 count = zeros(num_p,num_m,'single'); %for Netflida data, use sparse matrix instead. 
 
-movielens_data = dlmread('/Users/fpena/UCC/Thesis/datasets/uncompressed/ml-100k/u.data');
-disp(size(movielens_data,1));
-num_records = size(movielens_data,1);
-train_pct = 0.9;
-train_size = train_pct * size(movielens_data,1);
-disp(train_size);
-train_vec = movielens_data(1:train_size,1:3);
-probe_vec = movielens_data(train_size+1:end,1:3);
-disp('Num movies');
-disp(size(unique(train_vec(:,1))));
-disp(size(unique(probe_vec(:,2))));
-num_p = size(unique(movielens_data(:,2)));
-num_m = size(unique(movielens_data(:,1)));
-
 for mm=1:num_m
  ff= find(train_vec(:,2)==mm);
  count(train_vec(ff,1),mm) = train_vec(ff,3);
