6f64c69c45e6bcf6ce94e2cf9355892b165beeb5
franpena <fnapster@gmail.com>
2015-11-30 13:00:46 +0000

    Improved libfm_converter to handle multiple input files. In this way, one can pass a training and a test dataset and it will convert both of them to the libfm format while keeping track of the IDs that the training and testing file share

--- source/python/etl/libfm_converter.py
+++ source/python/etl/libfm_converter.py
@@ -1,94 +1,17 @@
 
 import csv
-from StringIO import StringIO
-import numpy
-from etl import ETLUtils
 
 __author__ = 'fpena'
 
 
-def csv_to_libfm2(
-        input_file, output_file, target_column, one_hot_columns,
-        delete_columns=None, delimiter=',', has_header=False, num_folds=None):
-
-    if delete_columns is None:
-        delete_columns = []
-
-    csv_reader = csv.reader(open(input_file, 'r'), delimiter=delimiter)
-
-    if has_header:
-        next(csv_reader)
-    data_matrix = list(csv_reader)
-
-    # The static columns are that ones that are not going to be deleted, don'
-    # belong to the one-hot columns or the target column
-    all_columns = range(len(data_matrix[0]))
-    static_columns = set(all_columns).difference(
-        [target_column], one_hot_columns, delete_columns)
-
-    id_counter = 0
-    output_rows = []
-
-    # Do the target column
-    for row_index in range(len(data_matrix)):
-        output_rows.append(str(data_matrix[row_index][target_column]))
-
-    # Do the static columns
-    for column_index in static_columns:
-        # column = data_matrix[:, column_index]
-        column = get_column(data_matrix, column_index)
-
-        for row_index in range(len(data_matrix)):
-            output_rows[row_index] += " " + str(id_counter) + ":" + str(column[row_index])
-
-        id_counter += 1
-
-    vector_map = {}
-
-    # Do the one-hot columns
-    for column_index in one_hot_columns:
-        # column = data_matrix[:, column_index]
-        column = get_column(data_matrix, column_index)
-
-        for row_index in range(len(data_matrix)):
-            vector_key = str(column_index) + " : " + str(column[row_index])
-
-            if vector_key not in vector_map:
-                vector_map[vector_key] = id_counter
-                id_counter += 1
-
-            output_rows[row_index] += " " + str(vector_map[vector_key]) + ":1"
-
-    if num_folds is None:
-        with open(output_file, 'w') as write_file:
-            for row in output_rows:
-                write_file.write("%s\n" % row)
-            return
-
-    for fold in range(num_folds):
-        split = 1 - (1/float(num_folds))
-        start = float(fold) / num_folds
-        train_records, test_records =\
-            ETLUtils.split_train_test(output_rows, split, False, start)
-
-        with open(output_file + "_train_" + str(fold), 'w') as write_file:
-            for row in train_records:
-                write_file.write("%s\n" % row)
-        with open(output_file + "_test_" + str(fold), 'w') as write_file:
-            for row in test_records:
-                write_file.write("%s\n" % row)
-
-
 def csv_to_libfm(
-        input_file, output_file, target_column, delete_columns=None,
-        delimiter=',', has_header=False):
+        input_files, target_column, one_hot_columns,
+        delete_columns=None, delimiter=',', has_header=False):
     """
     Converts a CSV file to the libFM format.
 
-    :type input_file: str
-    :param input_file: the path of the CSV file to convert
-    :type output_file: str
-    :param output_file: the path of the output libFM file
+    :type input_files: list[str]
+    :param input_files: a list with the path of the CSV files to convert
     :type target_column: int
     :param target_column: the index of the column that contains the target.
     In the case of a recommender system, the target is the rating.
@@ -101,46 +24,69 @@ def csv_to_libfm(
     :param has_header: a boolean indicating if the CSV file has a header or not
     """
 
-    id_counter = 0
+    if delete_columns is None:
+        delete_columns = []
 
-    with open(input_file, 'rb') as f:
+    csv_reader_list = [
+        csv.reader(open(input_file, 'r'), delimiter=delimiter)
+        for input_file in input_files
+        ]
 
-        reader = csv.reader(f, delimiter=delimiter)
+    data_matrix_list = []
 
+    for csv_reader in csv_reader_list:
         if has_header:
-            next(reader)
+            next(csv_reader)
+        data_matrix_list.append(list(csv_reader))
 
-        # target_list = []
-        if delete_columns is None:
-            delete_columns = []
-        delete_columns.append(target_column)
+    # The static columns are that ones that are not going to be deleted, don't
+    # belong to the one-hot columns or the target column
+    all_columns = range(len(data_matrix_list[0][0]))
+    static_columns = set(all_columns).difference(
+        [target_column], one_hot_columns, delete_columns)
 
-        vector_map = {}
+    id_counter = 0
+    output_row_list = []
+
+    # Do the target column
+    for data_matrix in data_matrix_list:
         output_rows = []
+        for row_index in range(len(data_matrix)):
+            output_rows.append(str(data_matrix[row_index][target_column]))
+        output_row_list.append(output_rows)
 
-        for row in reader:
+    # Do the static columns
+    for column_index in static_columns:
 
-            # Remove unwanted columns and the target (rating or dependent)
-            # variable
-            target = row[target_column]
-            for index in sorted(delete_columns, reverse=True):
-                row.pop(index)
+        for data_matrix, output_rows in zip(data_matrix_list, output_row_list):
+            column = get_column(data_matrix, column_index)
 
-            output_row = str(target)
+            for row_index in range(len(data_matrix)):
+                output_rows[row_index] +=\
+                    " " + str(id_counter) + ":" + str(column[row_index])
 
-            for i in range(len(row)):
-                vector_key = str(i) + " : " + row[i]
+        id_counter += 1
+
+    vector_map = {}
+
+    # Do the one-hot columns
+    for data_matrix, output_rows in zip(data_matrix_list, output_row_list):
+        for column_index in one_hot_columns:
+            column = get_column(data_matrix, column_index)
+
+            for row_index in range(len(data_matrix)):
+                vector_key = str(column_index) + " : " + str(column[row_index])
 
                 if vector_key not in vector_map:
                     vector_map[vector_key] = id_counter
                     id_counter += 1
 
-                output_row += " " + str(vector_map[vector_key]) + ":1"
-            output_rows.append(output_row)
+                output_rows[row_index] += " " + str(vector_map[vector_key]) + ":1"
 
-    with open(output_file, 'w') as write_file:
-        for row in output_rows:
-            write_file.write("%s\n" % row)
+    for input_file, output_rows in zip(input_files, output_row_list):
+        with open(input_file + '.libfm', 'w') as write_file:
+            for row in output_rows:
+                write_file.write("%s\n" % row)
 
 
 def get_column(matrix, i):
@@ -186,13 +132,18 @@ def main():
     #     csv_file, libfm_file, 2, delete_columns=None,
     #     delimiter=',', has_header=True)
 
-    csv_file = "/Users/fpena/UCC/Thesis/datasets/context/yelp_hotel_context_shuffled.csv"
+    csv_files = [
+        "/Users/fpena/UCC/Thesis/datasets/context/yelp_hotel_context_shuffled.csv"
+    ]
     libfm_file = "/Users/fpena/UCC/Thesis/datasets/context/yelp_hotel_context_shuffled.libfm"
-    csv_to_libfm2(csv_file, libfm_file, 0, [1, 2], [], ',', has_header=True, num_folds=5)
+    csv_to_libfm(csv_files, 0, [1, 2], [], ',', has_header=True)
 
-main()
+# main()
 
 # d = StringIO("0.4,21,72,33\n0.1,35,58,44\n0.9,18,71,33\n0.9,18,71,44")
-# csv_file = '/Users/fpena/tmp/libfm-1.42.src/scripts/sample.csv'
+csv_files = [
+    '/Users/fpena/tmp/libfm-1.42.src/scripts/sample.csv',
+    '/Users/fpena/tmp/libfm-1.42.src/scripts/sample2.csv'
+]
 # libfm_file = '/Users/fpena/tmp/libfm-1.42.src/scripts/sample.libfm'
-# csv_to_libfm2(csv_file, libfm_file, 1, [4], [2])
+csv_to_libfm(csv_files, 1, [4], [2])
