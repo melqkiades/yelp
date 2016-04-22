549c9dbc0cf1d4145d67326e232ed52c53023372
franpena <fnapster@gmail.com>
2016-04-22 16:37:35 +0100

    Did some cleaning

--- source/python/topicmodeling/context/lda_context_utils.py
+++ source/python/topicmodeling/context/lda_context_utils.py
@@ -145,33 +145,6 @@ def calculate_topic_weighted_frequency_complete(topic, reviews):
 
     return results
 
-
-def discover_topics(text_reviews, num_topics):
-
-    processed = create_bag_of_words(text_reviews)
-
-    dictionary = corpora.Dictionary(processed)
-    dictionary.filter_extremes(2, 0.6)
-
-    # dictionary = corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in processed]
-
-    # I can print out the documents and which is the most probable topics for
-    # each doc.
-    lda_model =\
-        ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=num_topics)
-    # corpus_lda = lda_model[corpus]
-    #
-    # for l, t in izip(corpus_lda, corpus):
-    #   print l, "#", t
-    # for l in corpus_lda:
-    #     print(l)
-    # for topic in lda_model.show_topics(num_topics=num_topics, num_words=50):
-    #     print(topic)
-
-    return lda_model
-
-
 def create_bag_of_words(document_list):
     """
     Creates a bag of words representation of the document list given. It removes
@@ -288,27 +261,3 @@ def sample_bag_of_words(review_bow, sampling_method, max_words=None):
             numpy.random.choice(review_bow[0], num_words, replace=False)
         ]
         return review_bow
-
-
-my_documents = [
-    "Human machine interface for lab abc computer applications",
-    "A survey of user opinion of computer system response time",
-    "The EPS user interface management system",
-    "System and human system engineering testing of EPS",
-    "Relation of user perceived response time to error measurement",
-    "The generation of random binary unordered trees",
-    "The intersection graph of paths in trees",
-    "Graph minors IV Widths of trees and well quasi ordering",
-    "Graph minors A survey"
-]
-
-# print(create_bag_of_words(my_documents))
-
-# my_bag_of_words = create_bag_of_words(my_documents)
-# my_topic_model = discover_topics(my_documents, 3)
-# my_dictionary = corpora.Dictionary(my_bag_of_words)
-# my_dictionary.filter_extremes(2, 0.6)
-# my_corpus = [my_dictionary.doc2bow(text) for text in my_bag_of_words]
-# print(my_topic_model[my_corpus])
-# for t in my_topic_model[my_corpus]:
-#     print(t)
--- source/python/topicmodeling/context/topic_latex_generator.py
+++ source/python/topicmodeling/context/topic_latex_generator.py
@@ -6,7 +6,6 @@ import nltk
 import re
 from pylatex.base_classes import CommandBase, Arguments
 from pylatex import Document, Section, Subsection, UnsafeCommand, Tabular
-from pylatex.utils import italic
 from pylatex.package import Package
 
 from topicmodeling.context.topic_model_analyzer import load_topic_model, \
@@ -57,7 +56,8 @@ def extract_topic_words(topic_model, topic_ids):
 
     topic_words_map = {}
     for topic_id in topic_ids:
-        probability_words = topic_model.print_topic(topic_id, topn=10).split(' + ')
+        probability_words =\
+            topic_model.print_topic(topic_id, topn=10).split(' + ')
         words = set([word.split('*')[1] for word in probability_words])
         topic_words_map[topic_id] = words
 
@@ -98,7 +98,8 @@ class TopicLatexGenerator:
         self.init_headers()
         self.init_topic_words()
         self.init_topic_ids()
-        new_comm = UnsafeCommand('newcommand', '\exampleCommand', options=4,
+        new_comm = UnsafeCommand(
+            'newcommand', '\exampleCommand', options=4,
             extra_arguments=r'\colorbox[rgb]{#1,#2,#3}{#4} \color{black}')
         self.doc.append(new_comm)
         new_comm2 = UnsafeCommand('tiny')
@@ -108,7 +109,7 @@ class TopicLatexGenerator:
         golden_ratio = 0.618033988749895
         hsv_tuples = [((x * golden_ratio) % 1.0, 0.5, 0.95)
                       for x in range(self.num_topics)]
-        self.rgb_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples)
+        self.rgb_tuples = map(lambda hsv: colorsys.hsv_to_rgb(*hsv), hsv_tuples)
 
         color_index = 0
         self.automatic_context_topic_colors = {}
@@ -271,216 +272,13 @@ class TopicLatexGenerator:
         return doc_parts
 
 
-
-def fill_topics_document(doc, lda_based_context):
-    new_comm = UnsafeCommand('newcommand', '\exampleCommand', options=4,
-        extra_arguments=r'\colorbox[rgb]{#1,#2,#3}{#4} \color{black}')
-    doc.append(new_comm)
-
-    num_cols = 5
-    N = 150
-    golden_ratio = 0.618033988749895
-    hsv_tuples = [((x * golden_ratio) % 1.0, 0.5, 0.95) for x in range(N)]
-    rgb_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples)
-    automatic_context_topic_colors = {}
-    manual_context_topic_colors = {}
-
-    new_comm2 = UnsafeCommand('tiny')
-    doc.append(new_comm2)
-
-    table_format = '|' + 'c|' * (num_cols + 1)
-
-    with doc.create(Section('Context-rich topic models (automatic)')):
-
-        num_context_topics = len(lda_based_context.context_rich_topics)
-
-        with doc.create(Tabular(table_format)) as table:
-            table.add_hline()
-
-            headers = ['ID']
-            for column_index in range(num_cols):
-                headers.append('Word ' + str(column_index + 1))
-            table.add_row(headers)
-            table.add_hline()
-
-            color_index = 0
-            for topic in lda_based_context.context_rich_topics:
-                topic_id = topic[0]
-                row = [str(topic_id + 1)]
-                topic_words = lda_based_context.topic_model.print_topic(topic_id, topn=num_cols).split(' + ')
-                row.extend(topic_words)
-                table.add_row(row)
-                automatic_context_topic_colors[topic_id] =\
-                    rgb_tuples[color_index]
-                color_index += 1
-
-            table.add_hline()
-
-        doc.append(UnsafeCommand('par'))
-        doc.append('Number of context-rich topics: %d' % num_context_topics)
-
-    with doc.create(Section('Context-rich topic models (manual)')):
-
-        num_context_topics = 0
-
-        with doc.create(Tabular(table_format)) as table:
-            table.add_hline()
-            headers = ['ID']
-            for column_index in range(num_cols):
-                headers.append('Word ' + str(column_index + 1))
-            table.add_row(headers)
-            table.add_hline()
-
-            color_index = 0
-            for topic_id in range(lda_based_context.num_topics):
-                topic_score = split_topic(
-                    lda_based_context.topic_model.print_topic(topic_id, topn=num_cols))
-                if topic_score['score'] > 0:
-                    row = [topic_id]
-                    for column_index in range(num_cols):
-                        row.append(topic_score['word' + str(column_index)])
-                    table.add_row(row)
-                    manual_context_topic_colors[topic_id] = color_index
-
-                num_context_topics += 1
-
-            table.add_hline()
-
-        doc.append(UnsafeCommand('par'))
-        doc.append('Number of context-rich topics: %d' % num_context_topics)
-
-    with doc.create(Section('Reviewseeeeeee!!!!!')):
-        with doc.create(Subsection('A subsection')):
-
-            another_text = UnsafeCommand(
-                arguments=Arguments(1.0, 0.5, 0.5, ' Hola '))
-
-            doc.append('Also some crazy characters: $&#{}')
-            doc.append(' and some other things')
-            for color in rgb_tuples:
-                red = color[0]
-                green = color[1]
-                blue = color[2]
-                doc.append(
-                    ColorBoxCommand(
-                        arguments=Arguments(red, green, blue, 'Hello World!')))
-
-        review_index = 0
-        for record in lda_based_context.records[:100]:
-            print(review_index)
-            with doc.create(Subsection('Review %d (%s)' %
-                ((review_index + 1), record[Constants.PREDICTED_CLASS_FIELD]))):
-                doc.append(record[Constants.TEXT_FIELD])
-            review_index += 1
-
-
-def fill_document(doc):
-    """Add a section, a subsection and some text to the document.
-    :param doc: the document
-    :type doc: :class:`pylatex.document.Document` instance
-    """
-
-    new_comm = UnsafeCommand('newcommand', '\exampleCommand', options=4,
-                             extra_arguments=r'\colorbox[rgb]{#1,#2,#3}{#4} \color{black}')
-    doc.append(new_comm)
-
-    with doc.create(Section('A section')):
-        doc.append('Some regular text and some ')
-        doc.append(italic('italic text. '))
-
-        with doc.create(Subsection('A subsection')):
-            doc.append('Also some crazy characters: $&#{}')
-
-    with doc.create(Section('The second section')):
-        doc.append('Text for the second section ')
-        doc.append(
-            ColorBoxCommand(arguments=Arguments(1.0, 0.5, 0.5, 'Hello World!')))
-
-
-
-
 def main():
-    # Basic document
-    # doc = Document('basic')
-    # fill_document(doc)
-    #
-    # doc.generate_pdf()
-    # doc.generate_tex()
-
-    # doc = Document(Constants.ITEM_TYPE + '-topic-models')
     lda_based_context = load_topic_model(0, 0)
-    # lda_based_context = None
-    # fill_topics_document(doc, lda_based_context)
-
-    # doc.generate_pdf()
-    # doc.generate_tex()
     topic_latex_generator = TopicLatexGenerator(lda_based_context)
     topic_latex_generator.generate_pdf()
-
-    # Document with `\maketitle` command activated
-    # doc = Document()
-
-
-    # doc.preamble.append(Command('title', 'Awesome Title'))
-    # doc.preamble.append(Command('author', 'Anonymous author'))
-    # doc.preamble.append(Command('date', NoEscape(r'\today')))
-    # doc.append(NoEscape(r'\maketitle'))
-    #
-    # # fill_document(doc)
-    #
-    # doc.generate_pdf('basic_maketitle', clean=False)
-    #
-    # # Add stuff to the document
-    # # with doc.create(Section('A second section')):
-    # #     doc.append('Some text.')
-    #
-    # doc.generate_pdf('basic_maketitle2')
-    # tex = doc.dumps()  # The document as string in LaTeX syntax
 
 start = time.time()
 main()
 end = time.time()
 total_time = end - start
 print("Total time = %f seconds" % total_time)
-
-# print(background_color_mapper('hola', (0.5, 0.7, 0.2)))
-
-
-def build_text(review, topic_words_map):
-    words = extract_words(review)
-    doc_parts = []
-    new_words = []
-    for word in words:
-        word_found = False
-        for topic_id in topic_words_map.keys():
-            if word in topic_words_map[topic_id]:
-                doc_parts.append(' '.join(new_words))
-                doc_parts.append('topic: %d word: %s' % (topic_id, word))
-                new_words = []
-                word_found = True
-                break
-        if not word_found:
-            new_words.append(word)
-    doc_parts.append(''.join(new_words))
-
-    return doc_parts
-
-
-
-
-
-# lda_based_context = load_topic_model(0, 0)
-# probability_words = lda_based_context.topic_model.print_topic(0, topn=10).split(' + ')
-# for probability_word in probability_words:
-#     print(probability_word.split('*')[1])
-# review = lda_based_context.records[0][Constants.TEXT_FIELD]
-# print(review)
-# topic_words_map = extract_topic_words(lda_based_context.topic_model, range(10))
-# print(detect_word_in_topic(topic_words_map, 'summer'))
-# print(extract_words(lda_based_context.records[0][Constants.TEXT_FIELD]))
-# print(build_text(lda_based_context.records[0][Constants.TEXT_FIELD], topic_words_map))
-
-
-# print(lda_based_context.topic_model.get_topic_terms(0))
-
-
