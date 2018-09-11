95321d70c14c987f18658d70b589dc251bc2a245
franpena <fnapster@gmail.com>
2018-09-11 21:29:48 +0100

    Added a function to plot the results of the recommendations in the thesis_charts module

--- source/python/visualization/thesis_charts.py
+++ source/python/visualization/thesis_charts.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import pandas
 import time
 
@@ -196,7 +198,79 @@ def plot_classifier_results():
     g.figure.savefig(file_name)
 
 
+def plot_recommender_results(evaluation_set, metric):
 
+    folder = '/tmp/results/'
+    file_path = folder + 'rival_%s_results_folds.csv' % Constants.ITEM_TYPE
+
+    data_frame = pandas.read_csv(file_path)
+
+    strategy = {'RMSE': 'user_test', 'Recall@10': 'rel_plus_n'}[metric]
+
+    data_frame = data_frame[data_frame['Strategy'] == strategy]
+    data_frame = data_frame[data_frame['Evaluation_Set'] == evaluation_set]
+    data_frame.sort_values('Num_Topics', inplace=True)
+    context_data_frame = data_frame[data_frame['Context_Format'] == 'context_topic_weights']
+    context_data_frame = context_data_frame[['Num_Topics', metric]]
+    no_context_data_frame = data_frame[data_frame['Context_Format'] == 'no_context']
+    no_context_data_frame = no_context_data_frame[['Num_Topics', metric]]
+
+    if metric == 'RMSE':
+        no_context_best_value = no_context_data_frame[metric].min()
+    elif metric == 'Recall@10':
+        no_context_best_value = no_context_data_frame[metric].max()
+    else:
+        raise ValueError('Unrecognized metric \'%s\'' % metric)
+
+    data_matrix = context_data_frame.values
+    num_topics_data = data_matrix[:, 0]
+    context_data = data_matrix[:, 1]
+    no_context_data = [no_context_best_value]*len(num_topics_data)
+
+    context_line, = plt.plot(num_topics_data, context_data, label='Rich Context')
+    no_context_line, = plt.plot(
+        num_topics_data, no_context_data, label='Factorization Machines',
+        linestyle='--')
+
+    # font = {'weight': 'bold',
+    #         'size': 22}
+
+    # matplotlib.rc('font', **font)
+
+    plt.legend(handles=[context_line, no_context_line], prop={'size': 14})
+    # plt.legend(handles=[context_line, no_context_line])
+
+
+    if metric == 'RMSE':
+        plt.ylim(0.9, 1.4)
+    if metric == 'Recall@10':
+        if Constants.ITEM_TYPE == 'yelp_hotel':
+            plt.ylim(0.0, 0.45)
+        else:
+            plt.ylim(0.0, 0.1)
+    plt.xlabel('Number of topics', fontsize=16)
+    plt.ylabel(metric, fontsize=16)
+    plt.xlabel('Number of topics')
+    plt.ylabel(metric)
+
+    # divergence = {'yelp_hotel': 9, 'yelp_restaurant': 34}[Constants.ITEM_TYPE]
+    # plt.axvline(x=divergence)
+    export_folder = '/tmp/'
+    file_path = export_folder + 'plot_%s_%s_%s.pdf' % (
+        metric, Constants.ITEM_TYPE, evaluation_set)
+    file_path = file_path.lower()
+    plt.savefig(file_path, figsize=(4, 4), bbox_inches='tight')
+    plt.cla()
+    plt.clf()
+
+
+    # seaborn.set_style("darkgrid")
+    # data_frame.plot.line(x='Num_Topics', y='RMSE', hue='Context_Format')
+    # plt.xlabel('Number of topics')
+    # plt.ylabel('Divergence')
+    # plt.ylim(0.9, 1.3)
+    #
+    # plt.savefig('/tmp/fourcity_hotel_rmse.pdf')
 
 
 def main():
@@ -207,8 +281,12 @@ def main():
     # plot_ats_score()
     # plot_adsd_score()
     # plot_divergence()
-    plot_classifier_results()
-
+    # plot_classifier_results()
+    evaluation_sets = ['test_users', 'test_only_users']
+    metrics = ['RMSE', 'Recall@10']
+    for evaluation_set, metric in product(evaluation_sets, metrics):
+        plot_recommender_results(evaluation_set, metric)
+    # plot_recommender_results('test_only_users')
 
 start = time.time()
 main()
